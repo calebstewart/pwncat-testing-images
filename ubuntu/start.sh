dc81357d75ba72bf06412d7c73eaea481f918223
Caleb Stewart <caleb.stewart94@gmail.com>
2021-05-30 01:04:00 -0400

    Fixed centos and ubuntu images

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -3,6 +3,9 @@
 # Start the SSH server
 /usr/sbin/sshd &
 
+# Ensure we are in a writeable directory
+cd /tmp
+
 # Start a service which can trigger reverse shells
 socat tcp-l:9999,fork,setsid,reuseaddr EXEC:/reverse.sh &
 
